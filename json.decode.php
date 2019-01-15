3a1fcfb0f0aae96a2dd7a1b6a1072676d659989c
neilventor <neilventor@yahoo.com>
2019-01-15 14:49:07 +0800

    fourth commit

---
+++ json.decode.php
@@ -0,0 +1,16 @@
+<?php
+    function w3rfunction($value,$key)
+    {
+        echo "$key : $value"."<br>";
+    }
+    $json = '{
+    "Title": "The Cuckoos Calling",
+    "Author": "Robert Galbraith",
+    "Detail":
+        {
+        "Publisher": "Little Brown"
+        }
+    }';
+$j1 = json_decode($json,true);
+array_walk_recursive($j1,"w3rfunction");
+?>
